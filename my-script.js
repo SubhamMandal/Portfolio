da57c201558739bd65301155e7ed9f8f42cd89e5
SubhamMandal <59119184+SubhamMandal@users.noreply.github.com>
2021-09-09 22:56:11 +0530

    Add files via upload

---
+++ my-script.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('.nav-button').forEach(item => {
+    item.addEventListener('click', () => {
+        console.log("worked");
+        document.getElementById("click").checked = false;
+    })
+});
+
+// document.getElementById('skills').addEventListener('click',()=>{
+//     window.scrollTo(0, 100);
+// })
+
+// Typing Effect 
+
+
+function wait(ms) {
+    var start = new Date().getTime();
+    var end = start;
+    while (end < start + ms) {
+        end = new Date().getTime();
+    }
+}
+var i = 0, j = 0;
+var txt = ['', 'software engineer!','Competitive coder','Web Developer']; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+function typeWriter() {
+    if (i < txt[j].length) {
+        document.getElementById("typing").innerHTML += txt[j].charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else if (j < txt.length - 1) {
+        wait(1000);
+        j++;
+        i = 0;
+        document.getElementById("typing").innerHTML = ".";
+        setTimeout(typeWriter, speed);
+    } else {
+        wait(1000);
+        document.getElementById("typing").innerHTML = ".";
+        j = 0;
+        i = 0;
+        setTimeout(typeWriter, speed);
+    }
+}
+setTimeout(typeWriter, 0);
