b78e7348acc375c33f6f2c351f047e3020d67a21
SubhamMandal <iamsubhammandal.sm@gmail.com>
2021-09-16 22:37:38 +0530

    about page - deatils tab

--- my-script.js
+++ my-script.js
@@ -49,3 +49,21 @@ function typeWriter() {
     }
 }
 setTimeout(typeWriter, 0);
+
+// Tabs Effect 
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+//   Show a tab by default
+document.getElementById("defaultOpen").click();
