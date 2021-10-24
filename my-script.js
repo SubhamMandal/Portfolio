e8b09505eb310e9d777aea8911f88bc000e9ac09
SubhamMandal <iamsubhammandal.sm@gmail.com>
2021-10-24 11:40:47 +0530

    fixed issues

--- my-script.js
+++ my-script.js
@@ -1,4 +1,4 @@
-window.onload = document.querySelector('.loader').style.display = "none";
+// window.onload = document.querySelector('.loader').style.display = "none";
 
 
 document.querySelectorAll('.nav-button').forEach(item => {
@@ -8,47 +8,35 @@ document.querySelectorAll('.nav-button').forEach(item => {
     })
 });
 
-// document.getElementById('skills').addEventListener('click',()=>{
-//     window.scrollTo(0, 100);
-// })
-
-// document.getElementById("click").addEventListener('click', () => {
-//     if (document.getElementById("click").checked == true) {
-//         document.getElementById("resume").classList.add("hide");
-//     }
-//     else document.getElementById("resume").classList.remove("hide");
-// })
-
 //============================= Typing Effect============================================== 
 
-// const blink = setInterval(()=>{   // for cursor blinking
-//     document.querySelector('.cursor').classList.toggle('hide');
-// },500);
-
-
 function wait(ms) {
-    var start = new Date().getTime();
-    var end = start;
-    while (end < start + ms) {
-        end = new Date().getTime();
-    }
-}
+    // const x = ms*j;
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+// function wait(ms) {
+//     var start = new Date().getTime();
+//     var end = start;
+//     while (end < start + ms) {
+//         end = new Date().getTime();
+//     }
+// }
 var i = 0, j = 0;
-var txt = ['', 'software engineer', 'Competitive coder', 'Web Developer']; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-function typeWriter() {
+var txt = [ 'software engineer', 'Competitive coder', 'Web Developer']; 
+var speed = 50; 
+async function typeWriter() {
     if (i < txt[j].length) {
         document.getElementById("typing").innerHTML += txt[j].charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     } else if (j < txt.length - 1) {
-        wait(3000);
+        await wait(3000);
         j++;
         i = 0;
         document.getElementById("typing").innerHTML = "";
         setTimeout(typeWriter, speed);
     } else {
-        wait(3000);
+        await wait(5000);
         document.getElementById("typing").innerHTML = "";
         j = 0;
         i = 0;
@@ -91,24 +79,6 @@ darkButton.onclick = () => {
 
 // ======================= project display slider =================
 
-// let projectOne = document.querySelector('#project1-img');
-// let projectTwo = document.querySelector('#project2-img');
-// let projectThree = document.querySelector('#project3-img');
-// let frame = document.querySelector('#frame').offsetWidth;
-// let index = 0;
-// let increment = 1;
-
-// function slide(card) {
-//     index = index + increment;
-//     if (index >= 2) increment = -1;
-//     if (index <= 0) increment = 1;
-//     card.style.transitionDuration = "0.5s";
-//     card.style.transform = `translateX(-${(frame + 40) * index}px)`;
-// }
-
-// setInterval(() => { slide(projectOne) }, 4000);
-// setTimeout(() => { setInterval(() => { slide(projectTwo) }, 4000) }, 2000);
-// setInterval(() => { slide(projectThree) }, 4000);
 
 let projectOne = document.querySelector("#project1-img");
 let projectTwo = document.querySelector("#project2-img");
